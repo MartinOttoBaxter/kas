a465a39bde1ec399956e3879d8a874c293e83e9f
Jan Kiszka <jan.kiszka@siemens.com>
2023-02-14 15:10:25 +0100

    tests: Cover caching of sha256sum calculations
    Validate that the checksum cache files are generated, contain the right
    values and also get invalidated again when the refspec changes.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- tests/test_sha256sum.py
+++ tests/test_sha256sum.py
@@ -23,6 +23,7 @@
 import os
 import pytest
 import shutil
+import sys
 from kas import kas
 
 
@@ -76,5 +77,49 @@ def test_invalid_sha256sum(changedir, tmpdir):
         recorder.buffer = ''
         kas.kas(['shell', 'test3.yml', '-c', 'true'])
         assert 'Checksum mismatch for repository hello' in recorder.buffer
+
+    sys.stderr = stderr
+
+
+def test_cache_sha256sum(changedir, tmpdir):
+    """
+        Test that checksums are chached but also correctly revalidated
+    """
+    tdir = str(tmpdir / 'test_valid_sha256sum')
+    shutil.copytree('tests/test_sha256sum', tdir)
+    os.chdir(tdir)
+
+    # first run
+    kas.kas(['shell', 'test1.yml', '-c', 'true'])
+    with open(tdir + '/kas.sha256sum', 'r') as f:
+        (cached_sha256sum, cached_refspec) = f.readline().split()
+    assert cached_sha256sum == \
+        'a66e3ff57062f39e182b2297f6b7ea3e35baa98579a40e8723ad06f6985fb3f6'
+    assert cached_refspec == '8a263b6ae455929f104a79039a76936552b382b3'
+    with open(tdir + '/hello.sha256sum', 'r') as f:
+        (cached_sha256sum, cached_refspec) = f.readline().split()
+    assert cached_sha256sum == \
+        '260402b2f9928f9d6ab878542d7e6e3c12daa39dd8e9eac60788022f3a73588a'
+    assert cached_refspec == '82e55d328c8c'
+
+    # cached run
+    recorder = OutputRecorder()
+    stderr = sys.stderr
+    sys.stderr = recorder
+
+    kas.kas(['shell', 'test1.yml', '-c', 'true'])
+
+    # check that there is any output
+    assert 'Repository kas already contains' in recorder.buffer
+    # ...but no validation
+    assert 'Checkout checksum of repository' not in recorder.buffer
+
+    # different refspec for kas; must trigger cache invalidation and fail
+    with pytest.raises(SystemExit):
+        recorder.buffer = ''
+        kas.kas(['shell', 'test2.yml', '-c', 'true'])
+    assert 'Checksum mismatch for repository kas' in recorder.buffer
+
+    assert not os.path.exists(tdir + '/kas.sha256sum')
 
     sys.stderr = stderr
